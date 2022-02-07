ec700a7fdf4e67de4ad548f3e2a1208e1e729081
耿耿耿1100 <3349495429@qq.com>
2022-02-07 22:49:00 +0800

    第二次提交2022-2-7

--- src/utils/tools.java
+++ src/utils/tools.java
@@ -3,8 +3,9 @@ package utils;
 import java.awt.*;
 
 public class tools {
-     static String path = "./src/image/";
+     static String path = "src/image/";
     static public Image hero_1 = Toolkit.getDefaultToolkit().createImage(path+"hero_1.png");
     static public Image zidan = Toolkit.getDefaultToolkit().createImage(path+"zidan.jpeg");
     static public Image bg = Toolkit.getDefaultToolkit().createImage(path+"bg.jpeg");
+
 }
