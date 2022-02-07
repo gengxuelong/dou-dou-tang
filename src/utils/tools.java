2ddb242cdd15a4f63ce2991127d40d95cebd1b37
耿耿耿1100 <3349495429@qq.com>
2022-02-07 23:00:32 +0800

    第二次提交2022-2-7

--- src/utils/tools.java
+++ src/utils/tools.java
@@ -1,11 +1,36 @@
 package utils;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.IOException;
 
 public class tools {
      static String path = "src/image/";
-    static public Image hero_1 = Toolkit.getDefaultToolkit().createImage(path+"hero_1.png");
-    static public Image zidan = Toolkit.getDefaultToolkit().createImage(path+"zidan.jpeg");
-    static public Image bg = Toolkit.getDefaultToolkit().createImage(path+"bg.jpeg");
+    static public Image hero_1; /*Toolkit.getDefaultToolkit().createImage(path+"hero_1.png")*/
+    static public Image zidan ;
+    static public Image bg ;
+    static {
+        try {
+            hero_1 = ImageIO.read(tools.class.getClassLoader().getResourceAsStream("image/hero_1.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    static {
+        try {
+            zidan = ImageIO.read(tools.class.getClassLoader().getResourceAsStream("image/zidan.jpeg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    static {
+        try {
+            bg = ImageIO.read(tools.class.getClassLoader().getResourceAsStream("image/bg.jpeg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
